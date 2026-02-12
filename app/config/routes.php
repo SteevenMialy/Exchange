2901c050476099dbe5b458e87736c16229f5d5f8
gracatanteraka <gracetanteraka@gmail.com>
2026-02-12 14:13:24 +0300

    route-home

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,8 +1,7 @@
 <?php
 
 use app\controllers\UserController;
-use app\controllers\AdminController;
-use app\controllers\ObjectController; 
+use app\controllers\ObjectController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
@@ -30,17 +29,12 @@ $router->group('', function (Router $router) use ($app) {
 	Flight::route('/api/validate/signin', [UserController::class, 'validateSignin']);
 	Flight::route('/signIn', [UserController::class, 'save']);
 
-	$router->get('/home', function ($id) use ($app) {
+	$router->get('/home', function () use ($app) {
+		$id = $_SESSION['user']->id ?? null;
 		$controller = new ObjectController($app);
 		$app->render('home', [
-			'objects' => "holla"
+			'objects' => ObjectController::getAllObjectUserCo($id)
 		]);
-	});
-
-	$router->post('/connect/admin', function () use ($app) {
-		$controller = new AdminController($app);
-		$controller->authenticateAdmin();
-
 	});
 
 	$router->get('/adminpage', function () use ($app) {
@@ -62,5 +56,4 @@ $router->group('', function (Router $router) use ($app) {
 	$router->get('/contact', function () use ($app) {
 		$app->render('contact');
 	});
-
 }, [SecurityHeadersMiddleware::class]);
