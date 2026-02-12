f7bb0261b3fd33c4cde4b12304774f4c201c6e3b
T5iory <t.niryavo@gmail.com>
2026-02-12 11:35:22 +0300

    scripttsiory

--- 12-02-2026-02-tsiory.sql
+++ 12-02-2026-02-tsiory.sql
@@ -2,16 +2,17 @@ CREATE TABLE exch_category (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nomcategory VARCHAR(255)
 );
+
 CREATE TABLE exch_object (
     id INT PRIMARY KEY AUTO_INCREMENT,
     id_user INT,
     id_category INT,
     descript TEXT,
-    prix NUMBER(10,2),
+    prix DECIMAL(10,2),
 
     CONSTRAINT fk_category 
     FOREIGN KEY (id_category) 
-    REFERENCES exch_category(id)
+    REFERENCES exch_category(id),
 
     CONSTRAINT fk_object_user
     FOREIGN KEY (id_user) 
